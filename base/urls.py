411bff6e570b98154c02f49122822dec96c10970
Akash Sikarwar <akashsikarwar2211@gmail.com>
2025-07-03 22:20:37 +0530

    adding new features to it.

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns= [
+    path('logout/',views.logoutUser, name="logout"),
     path('login/',views.loginPage, name="login"),
+    path('register/',views.registerPage, name="register"),
     path('',views.home,name="home"),
     path('room/<str:pk>/',views.room,name="room"),
     path('create-room/',views.createRoom, name="create-room"),
